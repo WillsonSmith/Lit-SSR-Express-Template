6b65000b1a2f9d842f79458804391b80702e83b4
Willson Smith <me@willsonsmith.com>
2023-02-04 15:42:02 -0500

    user create route

--- src/pages/admin/users/userEdit.html.ts
+++ src/pages/admin/users/userEdit.html.ts
@@ -1,4 +1,5 @@
 import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import prisma from '../../../db/client.js';
 import type { Role } from '@prisma/client';
 import type { UserWithRole } from '../../../types/User.js';
@@ -9,14 +10,15 @@ type Data = {
   action?: string;
 };
 
-export const page = ({ user, roles, action = '/admin/users/:id' }: Data) => {
+export const page = ({ user, roles, action }: Data) => {
+  const ActionType = user.id ? 'Update' : 'Create';
   return html`
     <sl-card>
       <div slot="header">
-        <h2>Edit user</h2>
+        <h2>${ActionType} user</h2>
       </div>
 
-      <form id="user-form" method="post" action=${action}>
+      <form id="user-form" method="post" action=${ifDefined(action)}>
         <form-layout columns=${2}>
           <sl-input
             label="Name"
@@ -35,10 +37,21 @@ export const page = ({ user, roles, action = '/admin/users/:id' }: Data) => {
           >
             ${roles.map(role => html` <sl-option value=${role.id}>${role.name}</sl-option> `)}
           </sl-select>
+
+          <sl-input
+            label="Password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            type="password"
+            help-text="Passwords are temporary."
+          ></sl-input>
         </form-layout>
       </form>
       <div slot="footer">
-        <sl-button type="submit" variant="primary" outline form="user-form">Update user</sl-button>
+        <sl-button type="submit" variant="primary" outline form="user-form"
+          >${ActionType} user</sl-button
+        >
       </div>
     </sl-card>
   `;
@@ -66,7 +79,28 @@ export const get = async (req, res) => {
     },
   });
   const roles = await prisma.role.findMany();
-  res.render('admin/users/userEdit', { authenticated: true, user, roles });
+
+  res.render('admin/users/userEdit', { action: req.path, authenticated: true, user, roles });
+};
+
+export const post = async (req, res) => {
+  const { id } = req.params;
+  const { name, role, password } = req.body;
+  await prisma.user.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: {
+      name,
+      role: {
+        connect: {
+          id: parseInt(role),
+        },
+      },
+      password,
+    },
+  });
+  res.redirect('/admin/users');
 };
 
 export { template } from './_template.js';
