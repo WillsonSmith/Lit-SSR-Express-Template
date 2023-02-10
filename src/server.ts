8955ac6745baecb22d63afc27b4f85fe94a0bf3d
Willson Smith <me@willsonsmith.com>
2023-02-10 17:22:34 -0500

    css imports working

--- src/server.ts
+++ src/server.ts
@@ -56,15 +56,13 @@ const nodeModules = join(__dirname, '..', 'node_modules');
 app.use('/public/shoelace', express.static(join(nodeModules, '@shoelace-style/shoelace')));
 
 const pagePaths = glob.sync(`${__dirname}/pages/**/*.*.js`);
-const staticRoutes = await import('./routes.js');
-for (const { path, route } of staticRoutes.routes) {
-  // this might not actually work, I may have to do it from within the build file.
-  // generate static routes for public files in relative directories
-  app.use(route, express.static(join(__dirname, path)));
-}
-
 for (const pagePath of pagePaths) {
-  const { route, get, post, middleware = [], handler, action } = await import(pagePath);
+  const { route, get, post, middleware = [], handler, action, styles } = await import(pagePath);
+
+  if (styles) {
+    const assetsPath = join(pagePath.split('/').slice(0, -1).join('/'), 'assets');
+    app.use(`${route}/assets`, express.static(assetsPath));
+  }
 
   const handlerMiddleware = async (req, res, next) => {
     req.locals ??= {};
